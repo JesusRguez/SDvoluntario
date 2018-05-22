1499c9cb963230e2b7a47dfbc35fe9848b817243
Jesús Rodríguez Heras <jesusrh1997@gmail.com>
2018-05-22 12:07:20 +0200

    Añadido comentario a Arantxa

--- bienvenida.java
+++ bienvenida.java
@@ -1,6 +1,7 @@
 public class bienvenida{
 	public static void main(String[] args) {
-		System.out.println("Bienvenido a este repositorio para SD holaaaaaaaaaaaaaaaaa");
+		System.out.println("Bienvenido a este repositorio para SD holaaaaaaaaaaaaaaaaa"); //La verdad es que Arantxa no sabe modificarlo bien
+		System.out.println("Hola Arantxa");
 		
 	}
 }
