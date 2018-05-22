1646b012bf1c6f16ebfbfde1f509a1043da91986
Arantxa <arantxaotal@gmail.com>
2018-05-22 11:58:08 +0200

    añadido hola

--- bienvenida.java
+++ bienvenida.java
@@ -1,5 +1,6 @@
 public class bienvenida{
 	public static void main(String[] args) {
-		System.out.println("Bienvenido a este repositorio para SD");
+		System.out.println("Bienvenido a este repositorio para SD holaaaaaaaaaaaaaaaaa");
+		
 	}
 }
