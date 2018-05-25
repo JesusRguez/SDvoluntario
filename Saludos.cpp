90aa8cc86d9607eb7a6d5a6653b14fd2ab3f8285
Jesús Rodríguez Heras <jesusrh1997@gmail.com>
2018-05-25 13:01:12 +0200

    Añadido el salto de linea

--- Saludos.cpp
+++ Saludos.cpp
@@ -3,7 +3,7 @@
 int main() {
 
 	std::cout << "Saludos mundo cruel" << std::endl;
-	std::cout << "Aunque no para todos es tan cruel :D"
+	std::cout << "Aunque no para todos es tan cruel :D" << std::endl;
 	return 0;
 
 }
